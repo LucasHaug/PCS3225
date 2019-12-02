d647d428f9cd50bee28c4f05667255c8f4569d2c
LucasHaug <lucas.haug@hotmail.com>
2019-12-01 23:58:42 -0300

    Finish EP5

--- EP5/polilegsc.vhd
+++ EP5/polilegsc.vhd
@@ -7,18 +7,18 @@
 library ieee;
 use ieee.numeric_bit.all;
 
-entity sift_left_2 is
+entity shift_left_2 is
     generic(word_size : natural := 64);
     port(
         input_to_be_shifted : in  bit_vector(word_size - 1 downto 0);
         shifted_output      : out bit_vector(word_size - 1 downto 0)
     );
-end entity sift_left_2;
+end entity shift_left_2;
 
-architecture sift_left_2_arch of sift_left_2 is
+architecture shift_left_2_arch of shift_left_2 is
 begin
-    shifted_output <= (input_to_be_shifted(word_size - 3) & "00");
-end architecture sift_left_2_arch;
+    shifted_output <= (input_to_be_shifted(word_size - 3 downto 0) & "00");
+end architecture shift_left_2_arch;
 
 -------------------------------- Mux 2 to 1 --------------------------------
 
@@ -280,6 +280,39 @@ begin
                     '1' when opcode = ADD_OP or opcode = SUB_OP or opcode = ORR_OP or opcode = AND_OP or opcode = LDUR_OP;
 end architecture controlunit_arch;
 
+-------------------------------- PC --------------------------------
+
+library ieee;
+use ieee.numeric_bit.all;
+
+entity PC is
+    generic(wordSize: natural := 64);
+    port(
+        clock           : in  bit; --! entrada de clock
+        reset           : in  bit; --! clear assincrono
+        load            : in  bit; --! write enable (carga paralela)
+        new_address     : in  bit_vector(wordSize - 1 downto 0); --! entrada
+        current_address : out bit_vector(wordSize - 1 downto 0)  --! saida
+    );
+end entity PC;
+
+architecture PC_arch of PC is
+    signal address : bit_vector(wordSize - 1 downto 0) := (others => '0');
+begin
+    PC_process : process(clock, reset)
+    begin
+        if (reset = '1') then
+            address <= (others => '0');
+        elsif (rising_edge(clock)) then
+            if (load = '1') then
+                address <= new_address;
+            end if;
+        end if;
+    end process PC_process;
+
+    current_address <= address;
+end architecture PC_arch;
+
 -------------------------------- Datapath --------------------------------
 
 library ieee;
@@ -307,16 +340,18 @@ entity datapath is
 
         -- IM interface
         imAddr : out bit_vector(63 downto 0);
-        imOut  : in bit_vector(31 downto 0);
+        imOut  : in  bit_vector(31 downto 0);
 
         -- DM interface
         dmAddr : out bit_vector(63 downto 0);
         dmIn   : out bit_vector(63 downto 0);
-        dmOut  : in bit_vector(63 downto 0)
+        dmOut  : in  bit_vector(63 downto 0)
     );
 end entity datapath ;
 
 architecture datapath_arch of datapath is
+    --------- Components ---------
+
     component regfile is
         generic(
             regn : natural := 32;
@@ -363,14 +398,104 @@ architecture datapath_arch of datapath is
         );
     end component mux2;
 
-    component sift_left_2 is
+    component shift_left_2 is
         generic(word_size : natural := 64);
         port(
             input_to_be_shifted : in  bit_vector(word_size - 1 downto 0);
             shifted_output      : out bit_vector(word_size - 1 downto 0)
         );
-    end component sift_left_2;
+    end component shift_left_2;
+
+    component PC is
+        generic(wordSize: natural := 64);
+        port(
+            clock           : in  bit; --! entrada de clock
+            reset           : in  bit; --! clear assincrono
+            load            : in  bit; --! write enable (carga paralela)
+            new_address     : in  bit_vector(wordSize - 1 downto 0); --! entrada
+            current_address : out bit_vector(wordSize - 1 downto 0)  --! saida
+        );
+    end component PC;
+
+    --------- Signals ---------
+
+    -- Registers
+
+    signal reg1_addr      : bit_vector(04 downto 0) := (others => '0');
+    signal reg2_addr      : bit_vector(04 downto 0) := (others => '0');
+    signal reg_to_wr_addr : bit_vector(04 downto 0) := (others => '0');
+    signal data_to_write  : bit_vector(63 downto 0) := (others => '0');
+    signal reg1_data      : bit_vector(63 downto 0) := (others => '0');
+    signal reg2_data      : bit_vector(63 downto 0) := (others => '0');
+
+    -- Instruction
+
+    signal instruction   : bit_vector(31 downto 0) := (others => '0');
+    signal extended_addr : bit_vector(63 downto 0) := (others => '0');
+
+    -- Main ALU
+
+    signal main_alu_result  : bit_vector(63 downto 0) := (others => '0');
+    signal main_alu_b_input : bit_vector(63 downto 0) := (others => '0');
+
+    -- Branch ALU
+
+    signal branch_alu_result  : bit_vector(63 downto 0) := (others => '0');
+    signal branch_alu_b_input : bit_vector(63 downto 0) := (others => '0');
+
+    -- PC
+
+    signal current_pc_value : bit_vector(63 downto 0) := (others => '0');
+    signal new_pc_value     : bit_vector(63 downto 0) := (others => '0');
+    signal pc_plus_4        : bit_vector(63 downto 0) := (others => '0');
 begin
+    -- PC
+
+    programm_counter : PC generic map (64) port map (clock, reset, '1', new_pc_value, current_pc_value);
+
+    -- Register file
+
+    registers_file : regfile generic map (32, 64) port map(clock, reset, regWrite, reg1_addr, reg2_addr, reg_to_wr_addr, data_to_write, reg1_data, reg2_data);
+
+    -- Sign Extend
+
+    dp_sign_extend : signExtend port map (instruction, extended_addr);
+
+    -- Shift Left 2
+
+    sl2: shift_left_2 generic map (64) port map (extended_addr, branch_alu_b_input);
+
+    -- ULAs
+
+    pc_adder   : alu generic map (64) port map(current_pc_value, X"0000000000000004", pc_plus_4, "0010", open, open, open);
+    branch_alu : alu generic map (64) port map(current_pc_value, branch_alu_b_input, branch_alu_result, "0010", open, open, open);
+    main_alu   : alu generic map (64) port map(reg1_data, main_alu_b_input, main_alu_result, aluCtrl, zero, open, open);
+
+    -- Muxes
+
+    pc_mux         : mux2 generic map (64) port map (pcsrc, pc_plus_4, branch_alu_result, new_pc_value);
+    instr_mux      : mux2 generic map (5) port map (reg2loc, instruction(20 downto 16), instruction(4 downto 0), reg2_addr);
+    mem_to_reg_mux : mux2 generic map (64) port map (memToReg, main_alu_result, dmOut, data_to_write);
+    alu_mux        : mux2 generic map (64) port map (aluSrc, reg2_data, extended_addr, main_alu_b_input);
+
+    -- Instruction
+
+    instruction <= imOut;
+
+    reg1_addr      <= instruction(9 downto 5);
+    reg_to_wr_addr <= instruction(4 downto 0);
+
+    imAddr <= current_pc_value;
+
+    opcode <= ("10110100" & "000") when instruction(31 downto 24) = "10110100" else
+              ("000101" & "00000") when instruction(31 downto 26) = "000101" else
+              instruction(31 downto 21);
+
+    -- Data Memory
+
+    dmAddr <= main_alu_result;
+    dmIn   <= reg2_data;
+
 end architecture datapath_arch;
 
 -------------------------------- PoliLEG --------------------------------
@@ -450,32 +575,62 @@ architecture polilegsc_arch of polilegsc is
         );
     end component controlunit;
 
+    -- Opcode
+
     signal opcode_pl       : bit_vector(10 downto 0) := (others => '0');
+
+    -- ALU
+
     signal aluCtrl_pl      : bit_vector(3 downto 0) := (others => '0');
     signal aluOp_pl        : bit_vector(1 downto 0) := (others => '0');
     signal aluSrc_pl       : bit := '0';
+
+    -- Registers
+
     signal reg2loc_pl      : bit := '0';
     signal regWrite_pl     : bit := '0';
+
+    -- Memory
+
     signal memToReg_pl     : bit := '0';
     signal memRead_pl      : bit := '0';
     signal memWrite_pl     : bit := '0';
+
+    -- Branch
+
     signal zero_pl         : bit := '0';
     signal branch_pl       : bit := '0';
     signal uncondBranch_pl : bit := '0';
-    signal pcsrc_pl        : bit := '0';
 
     signal should_branch_because_zero : bit := '0';
     signal should_branch : bit := '0';
 
+    -- PC
+
+    signal pcsrc_pl        : bit := '0';
+
 begin
+    -- Datapath
+
     polileg_dp: datapath port map(clock, reset, reg2loc_pl, pcsrc_pl, memToReg_pl, aluCtrl_pl, aluSrc_pl, regWrite_pl, opcode_pl, zero_pl, imem_addr, imem_data, dmem_addr, dmem_dati, dmem_dato);
+
+    -- Control Unit
+
     polileg_uc: controlunit port map(reg2loc_pl, uncondBranch_pl, branch_pl, memRead_pl, memToReg_pl, aluOp_pl, memWrite_pl, aluSrc_pl, regWrite_pl, opcode_pl);
+
+    -- ALU Control
+
     polileg_alucontrol: alucontrol port map(aluOp_pl, opcode_pl, aluCtrl_pl);
+
+    -- Data Memory Write Enable
 
     dmem_we <= '0' when memRead_pl = '1' else
                '1' when memWrite_pl = '1';
 
+    -- Branch
+
     should_branch_because_zero <= branch_pl and zero_pl;
     should_branch <= uncondBranch_pl or should_branch_because_zero;
     pcSrc_pl <= should_branch;
+
 end architecture polilegsc_arch;
